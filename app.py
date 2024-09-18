234158d7da9e5c01b3411354be897e472f8c5e03
SMuhammadMustafa <mmustafa1444@gmail.com>
2024-09-19 01:28:18 +0500

    feat: Add workflow to build and push on dockerhub refactor: Linting of app.py

--- app.py
+++ app.py
@@ -2,15 +2,17 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
-import matplotlib.pyplot as plt
-import pickle
 
 data = pd.read_csv('salary_data.csv')
 
-X = data['YearsExperience'].values.reshape(-1, 1)  # Independent variable (Years of Experience)
+# Independent variable (Years of Experience)
+X = data['YearsExperience'].values.reshape(-1, 1)
 y = data['Salary'].values  # Dependent variable (Salary)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X,
+                                                    y,
+                                                    test_size=0.2,
+                                                    random_state=42)
 
 model = LinearRegression()
 
